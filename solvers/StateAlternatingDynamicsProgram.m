487647202573bf7a9101d21e0ea632cf981f51d7
Andrés Valenzuela <avalenzu@mit.edu>
2015-07-21 07:40:21 -0400

    WIP

--- solvers/StateAlternatingDynamicsProgram.m
+++ solvers/StateAlternatingDynamicsProgram.m
@@ -3,16 +3,21 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
     I
     m
     N = 2
-    position_max = 1e3
+    position_max = 1e2
     velocity_max = 1e3
-    force_max = 1e2
+    force_max = 1e3
     dt = 1e-3
     w_fixed_array
     z_fixed_array
     F_fixed_array
     M_fixed_array
-    regions = struct('A', {}, 'b', {}, 'Aeq', {}, 'beq', {}, 'normal', {}, 'mu', {});
+    regions = struct('A', {}, 'b', {}, 'normal', {}, 'mu', {}, 'ncon', {});
     contact_pts
+    friction_cone_normals = [0.5, -0.25,  -0.25; ...
+                                 0,    0.433, -0.433; ...
+                                 0.5,  0.5,    0.5];
+    contact_point_slack_weight = 1;
+                              
   end
 
   methods
@@ -32,6 +37,7 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
     
     function obj = addDefaultConstraintsAndCosts(obj)
       
+      %obj = obj.addVariable('contact_point_slack', 'C', [1, 1], 0, 1);
       for i = 1:size(obj.contact_pts, 2)
         % Add binary variables for contact region assignment
         obj = obj.addVariable(sprintf('R%d',i), 'B', [numel(obj.regions), obj.N], 0, 1);
@@ -43,17 +49,18 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
         obj = obj.addVariable(sprintf('M%d',i), 'C', [3, obj.N], ...
           -obj.force_max, obj.force_max);
       end
-      obj = obj.addTranslationalDynamicConstraints(true);
-      obj = obj.addRotationalDynamicConstraints(true);
-      obj = obj.addContactPointConstraints(true);
-      obj = obj.addContactForceConstraints(true);
+      obj = obj.addTranslationalDynamicConstraints(false);
+      obj = obj.addRotationalDynamicConstraints(false);
+      obj = obj.addContactPointConstraints(false);
+      obj = obj.addContactForceConstraints(false);
       if obj.has_symbolic
         obj = obj.addSymbolicCost(sum((obj.vars.w.symb(:) - obj.w_fixed_array(:)).^2));
         obj = obj.addSymbolicCost(sum((obj.vars.z.symb(:) - obj.z_fixed_array(:)).^2));
-        for i = 1:size(obj.contact_pts,2)
-          obj = obj.addSymbolicCost(sum(sum((obj.vars.(sprintf('F%d',i)).symb - obj.F_fixed_array(:,:,i)).^2)));
-          obj = obj.addSymbolicCost(sum(sum((obj.vars.(sprintf('M%d',i)).symb - obj.M_fixed_array(:,:,i)).^2)));
-        end
+        %obj = obj.addSymbolicCost(obj.vars.contact_point_slack.symb);
+        %for i = 1:size(obj.contact_pts,2)
+          %obj = obj.addSymbolicCost(sum(sum((obj.vars.(sprintf('F%d',i)).symb - obj.F_fixed_array(:,:,i)).^2)));
+          %obj = obj.addSymbolicCost(sum(sum((obj.vars.(sprintf('M%d',i)).symb - obj.M_fixed_array(:,:,i)).^2)));
+        %end
       else
         Q = zeros(obj.nv);
         c = zeros(obj.nv, 1);
@@ -61,7 +68,9 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
         c(obj.vars.w.i(:)) = -2*obj.w_fixed_array(:);
         Q(obj.vars.z.i(:), obj.vars.z.i(:)) = eye(numel(obj.z_fixed_array));
         c(obj.vars.z.i(:)) = -2*obj.z_fixed_array(:);
+        %Q(obj.vars.contact_point_slack.i(:), obj.vars.contact_point_slack.i(:)) = obj.contact_point_slack_weight;
         alpha = sum(obj.w_fixed_array(:).^2) + sum(obj.z_fixed_array(:).^2);
+        %alpha = 0*alpha;
         obj = obj.addCost(Q, c, alpha);
       end
     end
@@ -97,27 +106,34 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
 %           obj = obj.addSymbolicCost(norm(v_next - v_next_desired));
         end
       else
+        offset = 0;
+        Aeq = zeros(6*obj.N-1, obj.nv);
+        beq = zeros(6*obj.N-1, 1);
         for n = 1:(obj.N-1)
-          F = m*g; %#ok
-          F_next = m*g; %#ok
-          Aeq = zeros(6, obj.nv);
-          beq = zeros(6, 1);
 
           % Position
-          % r_next - r - h*v = h^2/(2*m)*F
-          beq(1:3) = h^2/(2*m)*F; %#ok
-          Aeq(sub2ind(size(Aeq), 1:3, obj.vars.r.i(:,n+1)')) = 1;
-          Aeq(sub2ind(size(Aeq), 1:3, obj.vars.r.i(:,n)')) = -1;
-          Aeq(sub2ind(size(Aeq), 1:3, obj.vars.v.i(:,n)')) = -h;
+          % r_next - r - h*v = h^2/(2*m)*sum(Fi) + h^2*g/2
+          beq(1:3) = (h^2/2)*g;
+          % Executed in velocity section below
+          %for i = 1:size(obj.contact_pts, 2)
+            %Aeq(1:3, obj.vars.(sprintf('F%d',i)).i(:,n)) = -h^2/(2*m)*eye(3); %#ok
+          %end
+          Aeq(sub2ind(size(Aeq), offset+(1:3), obj.vars.r.i(:,n+1)')) = 1;
+          Aeq(sub2ind(size(Aeq), offset+(1:3), obj.vars.r.i(:,n)')) = -1;
+          Aeq(sub2ind(size(Aeq), offset+(1:3), obj.vars.v.i(:,n)')) = -h;
 
           % Velocity
-          % v_next - v = h/(2*m)*(F + F_next)
-          Aeq(sub2ind(size(Aeq), 4:6, obj.vars.v.i(:,n+1)')) = 1;
-          Aeq(sub2ind(size(Aeq), 4:6, obj.vars.v.i(:,n)')) = -1;
-          beq(sub2ind(size(Aeq), 4:6)) = h/(2*m)*(F+F_next); %#ok
-
-          obj = obj.addLinearConstraints([], [], Aeq, beq);
+          % v_next - v = h/(2*m)*sum(Fi+Fi_next) + h*g
+          Aeq(sub2ind(size(Aeq), offset+(4:6), obj.vars.v.i(:,n+1)')) = 1;
+          Aeq(sub2ind(size(Aeq), offset+(4:6), obj.vars.v.i(:,n)')) = -1;
+          for i = 1:size(obj.contact_pts, 2)
+            Aeq(offset+(1:3), obj.vars.(sprintf('F%d',i)).i(:,n)) = -h^2/(2*m)*eye(3); %#ok
+            Aeq(offset+(4:6), obj.vars.(sprintf('F%d',i)).i(:,n:n+1)) = -h/(2*m)*[eye(3), eye(3)]; %#ok
+          end
+          beq(offset+(4:6)) = h*g;
+          offset = offset + 6;
         end
+        obj = obj.addLinearConstraints([], [], Aeq, beq);
       end
     end
 
@@ -154,10 +170,6 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
                   + cross(obj.contact_pts(:, i), quatRotateVec(quatConjugate(z_fixed), obj.vars.(Fname).symb(:, n)));
             T_next = T_next + quatRotateVec(quatConjugate(z_next_fixed), obj.vars.(Mname).symb(:, n+1)) ...
                   + cross(obj.contact_pts(:, i), quatRotateVec(quatConjugate(z_next_fixed), obj.vars.(Fname).symb(:, n+1)));
-            T = T + 0.5*quatRotateVec(quatConjugate(z_fixed), obj.vars.(Mname).symb(:, n)) + 0.5*obj.quatRotateVec2(quatConjugate(z), quatConjugate(z_fixed), M_fixed(:, n)) ...
-                  + cross(obj.contact_pts(:, i), 0.5*quatRotateVec(quatConjugate(z_fixed), obj.vars.(Fname).symb(:, n)) + 0.5*obj.quatRotateVec2(quatConjugate(z), quatConjugate(z_fixed), F_fixed(:, n)));
-            T_next = T_next + 0.5*quatRotateVec(quatConjugate(z_next_fixed), obj.vars.(Mname).symb(:, n+1)) + 0.5*obj.quatRotateVec2(quatConjugate(z_next), quatConjugate(z_next_fixed), M_fixed(:, n+1)) ...
-                  + cross(obj.contact_pts(:, i), 0.5*quatRotateVec(quatConjugate(z_next_fixed), obj.vars.(Fname).symb(:, n+1)) + 0.5*obj.quatRotateVec2(quatConjugate(z_next), quatConjugate(z_next_fixed), F_fixed(:, n+1)));
           end
           Y_next_desired = quatRotateVec(A, ...
             quatRotateVec(A, I*w + h/2*T) ...
@@ -167,6 +179,10 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
           obj = obj.addSymbolicConstraints(Y_next == Y_next_desired);
         end
       else
+        Aeq = zeros(7*obj.N-1, obj.nv);
+        beq = zeros(7*obj.N-1, 1);
+
+        offset = 0;
         for n = 1:(obj.N-1)
           T = zeros(3, 1);
           T_next = zeros(3, 1);
@@ -176,56 +192,102 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
           w_next_fixed = obj.w_fixed_array(:, n+1);
           w_mid_fixed = 0.5*(w_fixed + w_next_fixed);
 
-          Aeq = zeros(7, obj.nv);
-          beq = zeros(7, 1);
-
           % quatProduct(z, expmap(h*w_mid_fixed)) - z_next= 0
           deltaZ = expmap2quat(h*w_mid_fixed);
-          Aeq(1:4, obj.vars.z.i(:,n+1)) = -eye(4);
-          Aeq(1, obj.vars.z.i(1,n)) = deltaZ(1);
-          Aeq(1, obj.vars.z.i(2:4,n)) = -deltaZ(2:4);
-          Aeq(2, obj.vars.z.i(1:3,n)) = deltaZ([2,1,4]);
-          Aeq(2, obj.vars.z.i(4,n)) = -deltaZ(3);
-          Aeq(3, obj.vars.z.i([1,3,4],n)) = deltaZ([3,1,2]);
-          Aeq(3, obj.vars.z.i(2,n)) = -deltaZ(4);
-          Aeq(4, obj.vars.z.i([1,2,4],n)) = deltaZ([4,3,1]);
-          Aeq(4, obj.vars.z.i(3,n)) = -deltaZ(2);
+          Aeq(offset+(1:4), obj.vars.z.i(:,n+1)) = -eye(4);
+          Aeq(offset+1, obj.vars.z.i(1,n)) = deltaZ(1);
+          Aeq(offset+1, obj.vars.z.i(2:4,n)) = -deltaZ(2:4);
+          Aeq(offset+2, obj.vars.z.i(1:3,n)) = deltaZ([2,1,4]);
+          Aeq(offset+2, obj.vars.z.i(4,n)) = -deltaZ(3);
+          Aeq(offset+3, obj.vars.z.i([1,3,4],n)) = deltaZ([3,1,2]);
+          Aeq(offset+3, obj.vars.z.i(2,n)) = -deltaZ(4);
+          Aeq(offset+4, obj.vars.z.i([1,2,4],n)) = deltaZ([4,3,1]);
+          Aeq(offset+4, obj.vars.z.i(3,n)) = -deltaZ(2);
 
           % R(A)'*I*w_next - R(A)*I*w = h/2*(R(A)*T + R(B)*T_next)
+          % where
+          %   T = sum( R(z_f)'*Mi + pt_i x R(z_f)'*Fi )
           RA = quat2rotmat(expmap2quat(-h/2*w_mid_fixed));
           RB = quat2rotmat(expmap2quat(h/2*w_mid_fixed));
-          Aeq(5:7, obj.vars.w.i(:,n+1)) = RA'*I; %#ok
-          Aeq(5:7, obj.vars.w.i(:,n)) = -RA*I; %#ok
-          beq(5:7) = h/2*(RA*T + RB*T_next);
-          obj = obj.addLinearConstraints([], [], Aeq, beq);
+          R_world_to_body = quat2rotmat(z_fixed)';
+          R_world_to_body_next = quat2rotmat(z_next_fixed)';
+          for i = 1:size(obj.contact_pts,2)
+            pt_cross = vectorToSkewSymmetric(obj.contact_pts(:,i));
+            Aeq(offset + (5:7), obj.vars.(sprintf('M%d',i)).i(:,n)) = -h/2*RA*R_world_to_body;
+            Aeq(offset + (5:7), obj.vars.(sprintf('F%d',i)).i(:,n)) = -h/2*RA*pt_cross*R_world_to_body;
+            Aeq(offset + (5:7), obj.vars.(sprintf('M%d',i)).i(:,n+1)) = -h/2*RB*R_world_to_body_next;
+            Aeq(offset + (5:7), obj.vars.(sprintf('F%d',i)).i(:,n+1)) = -h/2*RB*pt_cross*R_world_to_body_next;
+          end
+          Aeq(offset + (5:7), obj.vars.w.i(:,n+1)) = RA'*I; %#ok
+          Aeq(offset + (5:7), obj.vars.w.i(:,n)) = -RA*I; %#ok
+          beq(offset + (5:7)) = h/2*(RA*T + RB*T_next);
+          offset = offset + 7;
         end
+        obj = obj.addLinearConstraints([], [], Aeq, beq);
       end
     end
 
     function obj = addContactPointConstraints(obj, use_symbolic)
+      z_fixed = obj.z_fixed_array;
       if use_symbolic
         r = obj.vars.r.symb;
         z = obj.vars.z.symb;
-        z_fixed = obj.z_fixed_array;
         for i = 1:size(obj.contact_pts, 2)
           R = obj.vars.(sprintf('R%d',i)).symb;
           obj = obj.addSymbolicConstraints(sum(R,1) == 1);
           for n = 1:obj.N
-            p = obj.quatRotateVec2(z(:,n), z_fixed(:,n), obj.contact_pts(:,i)) + r(:,n);
+            %p = obj.quatRotateVec2(z(:,n), z_fixed(:,n), obj.contact_pts(:,i)) + r(:,n);
+            p = quatRotateVec(z_fixed(:,n), obj.contact_pts(:,i)) + r(:,n);
             for j = 1:numel(obj.regions)
               if ~isempty(obj.regions(j).A)
                 obj = obj.addSymbolicConstraints(implies(R(j,n), ...
                   obj.regions(j).A*p <= obj.regions(j).b));
               end
               if ~isempty(obj.regions(j).Aeq)
-                obj = obj.addSymbolicConstraints(implies(R(j, n), ...
-                  obj.regions(j).Aeq*p == obj.regions(j).beq));
+                error('No longer supported');
+                %obj = obj.addSymbolicConstraints(implies(R(j, n), ...
+                  %obj.regions(j).Aeq*p == obj.regions(j).beq));
               end
             end
           end
         end
       else
-        error('not implemented')
+        
+        n_contacts = size(obj.contact_pts, 2);
+        M = obj.position_max;
+
+        Aeq = zeros(n_contacts*obj.N, obj.nv);
+        beq = zeros(n_contacts*obj.N, 1);
+        A = zeros(n_contacts*sum([obj.regions.ncon]), obj.nv);
+        b = zeros(n_contacts*sum([obj.regions.ncon]), 1);
+        offset = 0;
+        offset_eq = 0;
+        for i = 1:size(obj.contact_pts, 2)
+          for n = 1:obj.N
+            % sum(R) == 1
+            Aeq(offset_eq+1, obj.vars.(sprintf('R%d',i)).i(:,n)) = 1;
+            beq(offset_eq+1) = 1;
+            offset_eq = offset_eq + 1;
+            for j = 1:numel(obj.regions)
+              if ~isempty(obj.regions(j).A)
+                % R(j,n) -> A*p <= b
+                % formulated as
+                % A*p <= b + M*(1 - R(j,n))
+                ncon = size(obj.regions(j).A, 1);
+                indices = offset + (1:ncon);
+                A(indices, obj.vars.r.i(:,n)) = obj.regions(j).A;
+                A(indices, obj.vars.(sprintf('R%d',i)).i(j,n)) = M*ones(ncon,1);
+                %A(indices, obj.vars.contact_point_slack.i) = -1*ones(ncon,1);
+                b(indices) = obj.regions(j).b - obj.regions(j).A ...
+                                        *quatRotateVec(z_fixed(:,n), ...
+                                                       obj.contact_pts(:,i)) ...
+                                     + M;
+                offset = offset + ncon;
+              end
+            end
+          end
+        end
+        obj = obj.addLinearConstraints(A, b, Aeq, beq);
       end
     end
     
@@ -240,8 +302,10 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
         for i = 1:size(obj.contact_pts, 2)
           R = obj.vars.(sprintf('R%d',i)).symb;
           for n = 1:obj.N-1
-            p = obj.quatRotateVec2(z(:,n), z_fixed(:,n), obj.contact_pts(:,i)) + r(:,n);
-            p_next = obj.quatRotateVec2(z(:,n+1), z_fixed(:,n+1), obj.contact_pts(:,i)) + r(:,n+1);
+            %p = obj.quatRotateVec2(z(:,n), z_fixed(:,n), obj.contact_pts(:,i)) + r(:,n);
+            %p_next = obj.quatRotateVec2(z(:,n+1), z_fixed(:,n+1), obj.contact_pts(:,i)) + r(:,n+1);
+            p = quatRotateVec(z_fixed(:,n), obj.contact_pts(:,i)) + r(:,n);
+            p_next = quatRotateVec(z_fixed(:,n+1), obj.contact_pts(:,i)) + r(:,n+1);
             pd = v(:,n) + cross(w(:,n), obj.contact_pts(:,i));
             F = obj.vars.(sprintf('F%d', i)).symb(:, n);
             M = obj.vars.(sprintf('M%d', i)).symb(:, n);
@@ -257,19 +321,105 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
                 pd_normal = dot(pd, normal)*normal;
                 pd_tan = pd - pd_normal;
                 % HACK
-                obj = obj.addSymbolicConstraints(cone(F_tan, mu*dot(F, obj.regions(j).normal)));
+                %obj = obj.addSymbolicConstraints(cone(F_tan, mu*dot(F, obj.regions(j).normal)));
+                obj = obj.addSymbolicConstraints(implies(R(j,n+1), ...
+                                                 obj.friction_cone_normals'*F >= 0));
                 obj = obj.addSymbolicConstraints(implies(R(j,n+1), M == 0));
                 % END_HACK
 %                 obj = obj.addSymbolicConstraints(implies(R(j,n) + R(j,n+1) == 2, pd == 0));
-                obj = obj.addSymbolicConstraints(implies(R(j,n) + R(j,n+1) == 2, p == p_next));
+                %obj = obj.addSymbolicConstraints(implies(R(j,n) + R(j,n+1) == 2, p == p_next));
 %                 obj = obj.addSymbolicConstraints(implies(R(j,n), p == p_next));
-%                 obj = obj.addSymbolicConstraints(implies(R(j,n), pd_tan == 0));
+                 obj = obj.addSymbolicConstraints(implies(R(j,n), pd_tan == 0));
               end
             end
           end
         end
       else
-        error('not implemented')
+        big_M = obj.force_max;
+        n_free_regions = sum(cellfun(@isempty, {obj.regions.normal}));
+        n_contact_regions = numel(obj.regions) - n_free_regions;
+        n_contacts = size(obj.contact_pts, 2);
+        n_fc_faces = size(obj.friction_cone_normals,2);
+        ncon_total = (obj.N-1)*n_contacts*(12*n_free_regions + (12 + n_fc_faces)*n_contact_regions);
+        A = zeros(ncon_total, obj.nv);
+        b = zeros(ncon_total, 1);
+        offset = 0;
+        for i = 1:size(obj.contact_pts, 2)
+          pt_cross = vectorToSkewSymmetric(obj.contact_pts(:,i));
+          for n = 1:obj.N-1
+            for j = 1:numel(obj.regions)
+              normal = obj.regions(j).normal;
+              mu = obj.regions(j).mu;
+              if isempty(normal)
+                % R(j,n+1) -> F <= 0 && 0 <= F
+                % formulated as
+                % F <= big_M*(1 - R(j,n))
+                % and
+                % -F <= big_M*(1 - R(j,n));
+                ncon = 6;
+                indices = offset + (1:ncon);
+                A(indices, obj.vars.(sprintf('R%d',i)).i(j,n+1)) = big_M*ones(6,1);
+                A(indices, obj.vars.(sprintf('F%d',i)).i(:,n)) = [eye(3); -eye(3)];
+                b(indices) = big_M;
+                offset = offset + ncon;
+
+                % R(j,n+1) -> M <= 0 && 0 <= M
+                % formulated as
+                % M <= big_M*(1 - R(j,n))
+                % and
+                % -M <= big_M*(1 - R(j,n));
+                ncon = 6;
+                indices = offset + (1:ncon);
+                A(indices, obj.vars.(sprintf('R%d',i)).i(j,n+1)) = big_M*ones(6,1);
+                A(indices, obj.vars.(sprintf('M%d',i)).i(:,n)) = [eye(3); -eye(3)];
+                %A(indices, obj.vars.contact_point_slack.i) = -1*ones(ncon,1);
+                b(indices) = big_M;
+                offset = offset + ncon;
+              else
+                % HACK
+                % R(j,n+1) -> -fc_normals'*F <= 0
+                % formulated as
+                % -fc_normals'*F <= big_M*(1 - R(j, n+1))
+                fc_normals = obj.regions(j).friction_cone_normals;
+                ncon = size(fc_normals, 2);
+                indices = offset + (1:ncon);
+                A(indices, obj.vars.(sprintf('R%d',i)).i(j, n+1)) = big_M*ones(ncon, 1);
+                A(indices, obj.vars.(sprintf('F%d',i)).i(:, n)) = -fc_normals';
+                b(indices) = big_M*ones(ncon, 1);
+                offset = offset + ncon;
+
+                ncon = 6;
+                indices = offset + (1:ncon);
+                A(indices, obj.vars.(sprintf('R%d',i)).i(j,n+1)) = big_M*ones(6,1);
+                A(indices, obj.vars.(sprintf('M%d',i)).i(:,n)) = [eye(3); -eye(3)];
+                b(indices) = big_M;
+                offset = offset + ncon;
+                % END_HACK
+                % R(j,n) -> (dp/dt)_tangential == 0
+                % <=>
+                % R(j,n) -> (v + w x pt_i)_tangential == 0
+                % <=>
+                % R(j,n) -> (v - pt_i x w) - normal'*(v - pt_i x w)*normal == 0
+                % <=>
+                % R(j,n) -> (I3 - normal*normal')*(v - pt_i x w) == 0
+                % formulated as
+                %  (I3 - normal*normal')*(v - pt_i x w) <= big_M*(1 - R(j,n))
+                % -(I3 - normal*normal')*(v - pt_i x w) <= big_M*(1 - R(j,n))
+                big_M = obj.velocity_max;
+                ncon = 6;
+                indices = offset + (1:ncon);
+                C = eye(3) - normal*normal';
+                A(indices, obj.vars.(sprintf('R%d',i)).i(j,n)) = big_M*ones(6,1);
+                A(indices, obj.vars.v.i(:,n)) = [C; -C];
+                A(indices, obj.vars.w.i(:,n)) = -[C; -C]*pt_cross;
+                %A(indices, obj.vars.contact_point_slack.i) = -1*ones(ncon,1);
+                b(indices) = big_M*ones(ncon, 1);
+                offset = offset + ncon;
+              end
+            end
+          end
+        end
+        obj = obj.addLinearConstraints(A, b, [], []);
       end
     end
     
@@ -289,10 +439,12 @@ classdef StateAlternatingDynamicsProgram < MixedIntegerConvexProgram
       end
       if ~isempty(normal)
         obj.regions(j).normal = normal;
+        obj.regions(j).friction_cone_normals = obj.friction_cone_normals;
       end
       if ~isempty(mu)
         obj.regions(j).mu = mu;
       end
+      obj.regions(j).ncon = size(obj.regions(j).A, 1);
     end
     
     function obj = addContactPoint(obj, p)
--- solvers/test/testStateAlternatingDynamicsProgram.m
+++ solvers/test/testStateAlternatingDynamicsProgram.m
@@ -1,47 +1,65 @@
-options.floating = 'quat';
-options.use_new_kinsol = true;
-urdf = fullfile(getDrakePath(), 'systems', 'plants', 'test', 'FallingBrick.urdf');
-rbm = RigidBodyManipulator(urdf, options);
-v = rbm.constructVisualizer();
-I = diag([1;6;3]);
-m = 1;
-N = 20;
-M = 1000;
-dt = 1/N;
-r0 = [0; 0; 1];
-z0 = rpy2quat([pi/3; 0; 0]);
-w0 = 0*[0; 1; 0];
-v0 = [0; 0; 0];
-w_fixed_array = 0*rand(3, N);
-z_fixed_array = repmat(z0, 1, N);
-M_fixed_array = zeros(3, N, 8);
-F_fixed_array = zeros(3, N, 8);
-lam = 0.5;
-tol = 1e-6;
+if ~exist('continuation', 'var') || ~continuation
+  options.floating = 'quat';
+  options.use_new_kinsol = true;
+  urdf = fullfile(getDrakePath(), 'systems', 'plants', 'test', 'FallingBrick.urdf');
+  rbm = RigidBodyManipulator(urdf, options);
+  v = rbm.constructVisualizer();
+  I = diag([1;6;3]);
+  m = 1;
+  N = 20;
+  M = 1000;
+  dt = 2/N;
+  r0 = [0; 0; 0.5];
+  z0 = rpy2quat([pi/2; 0; 0]);
+  w0 = [1e-3; 0; 8];
+  v0 = [0; 0; 8];
+  w_fixed_array = 0*rand(3, N);
+  z_fixed_array = repmat(z0, 1, N);
+  M_fixed_array = zeros(3, N, 8);
+  F_fixed_array = zeros(3, N, 8);
+  lam = 0.5;
+  tol = 1e-6;
+end
 
 total_solvertime = 0;
 total_time = tic;
 for i = 1:M
-  prog = StateAlternatingDynamicsProgram(I, m, N, dt, z_fixed_array, w_fixed_array, F_fixed_array, M_fixed_array, true);
+  prog = StateAlternatingDynamicsProgram(I, m, N, dt, z_fixed_array, w_fixed_array, F_fixed_array, M_fixed_array, false);
+  prog.contact_point_slack_weight = 1e3;
   prog.contact_pts = rbm.getTerrainContactPoints().pts;
   prog = prog.addRegion([0, 0, -1], 0, [], [], [], []);
-  prog = prog.addRegion([], [], [0, 0, 1], 0, [0; 0; 1], 1);
+  %prog = prog.addRegion([0, 0, 1], 0, [], [], [], []);
+  %prog = prog.addRegion([0, 0, 1; 0, 0, -1], [0; 0.06], [], [], [0; 0; 1], 1);
+  prog = prog.addRegion([0, 0, 1; 0, 0, -1], [0; 0.1], [], [], [0; 0; 1], 1);
   prog = prog.addDefaultConstraintsAndCosts();
   %prog = prog.addOrientationConstraint(1, [1; 0; 0; 0]);
   prog = prog.addOrientationConstraint(1, z0);
-  prog = prog.addPositionConstraint(1, r0, r0);
+  prog = prog.addPositionConstraint(1, r0 - [0; 0; 0.0], r0 + [0; 0; 0.8]);
+  %prog = prog.addSymbolicConstraints(0.9 <= prog.vars.r.symb(3,1) <= 1.1);
+  %prog = prog.addSymbolicConstraints(prog.vars.r.symb(1:2,1) == 0);
   prog = prog.addAngularVelocityConstraint(1, w0, w0);
   prog = prog.addVelocityConstraint(1, v0, v0);
-  [prog, solvertime, objval] = prog.solve();
+  for j = 1:size(prog.contact_pts,2)
+    prog.vars.(sprintf('F%d',j)).lb(:,prog.N) = 0;
+    prog.vars.(sprintf('F%d',j)).ub(:,prog.N) = 0;
+    prog.vars.(sprintf('M%d',j)).lb(:,prog.N) = 0;
+    prog.vars.(sprintf('M%d',j)).ub(:,prog.N) = 0;
+  end
+  params = struct();
+  %params.mipgap = 0.5;
+  [prog, solvertime, objval] = prog.solveGurobi(params);
   delta_norm = sum((w_fixed_array(:) - prog.vars.w.value(:)).^2) ...
                 + sum((z_fixed_array(:) - prog.vars.z.value(:)).^2);
   total_solvertime = total_solvertime + solvertime;
   w_fixed_array = (1-lam)*w_fixed_array + lam*prog.vars.w.value;
   z_fixed_array = (1-lam)*z_fixed_array + lam*prog.vars.z.value;
-  for j = 1:size(prog.contact_pts,2)
-    F_fixed_array(:, :, j) = (1-lam)*F_fixed_array(:, :, j) + lam*prog.vars.(sprintf('F%d',j)).value;
-    M_fixed_array(:, :, j) = (1-lam)*M_fixed_array(:, :, j) + lam*prog.vars.(sprintf('M%d',j)).value;
-  end
+  z_fixed_array = bsxfun(@rdivide, z_fixed_array, sqrt(sum(z_fixed_array.^2, 1)));
+  %for j = 1:size(prog.contact_pts,2)
+    %delta_norm = delta_norm + sum(sum((F_fixed_array(:, :, j) - prog.vars.(sprintf('F%d',j)).value).^2));
+    %delta_norm = delta_norm + sum(sum((M_fixed_array(:, :, j) - prog.vars.(sprintf('M%d',j)).value).^2));
+    %F_fixed_array(:, :, j) = (1-lam)*F_fixed_array(:, :, j) + lam*prog.vars.(sprintf('F%d',j)).value;
+    %M_fixed_array(:, :, j) = (1-lam)*M_fixed_array(:, :, j) + lam*prog.vars.(sprintf('M%d',j)).value;
+  %end
   fprintf('Objective Value: %f\tNorm squared of delta: %f\n', objval, delta_norm);
   t = cumsum([0, repmat(prog.dt, [1, prog.N-1])]);
   position_traj = PPTrajectory(foh(t, [prog.vars.r.value; prog.vars.z.value]));
