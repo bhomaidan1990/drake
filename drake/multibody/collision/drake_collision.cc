66e21ec301c28a7796e45e4d8096d367d6802444
Andrés Valenzuela <andres.valenzuela@tri.global>
2017-05-25 10:26:03 -0400

    CATCHALL

--- drake/multibody/collision/drake_collision.cc
+++ drake/multibody/collision/drake_collision.cc
@@ -2,22 +2,14 @@
 
 #include "drake/multibody/collision/collision_filter.h"
 
-#ifdef BULLET_COLLISION
-#include "drake/multibody/collision/bullet_model.h"
-#else
-#include "drake/multibody/collision/unusable_model.h"
-#endif
+#include "drake/multibody/collision/fcl_model.h"
 
 using std::unique_ptr;
 
 namespace DrakeCollision {
 
 unique_ptr<Model> newModel() {
-#ifdef BULLET_COLLISION
-  return unique_ptr<Model>(new BulletModel());
-#else
-  return unique_ptr<Model>(new UnusableModel());
-#endif
+  return unique_ptr<Model>(new FCLModel());
 }
 
 }  // namespace DrakeCollision
--- drake/multibody/collision/fcl_model.cc
+++ drake/multibody/collision/fcl_model.cc
@@ -1,39 +1,74 @@
 #include "drake/multibody/collision/fcl_model.h"
 
 #include <fcl/fcl.h>
+#include <Eigen/Dense>
 
 #include "drake/common/drake_assert.h"
 #include "drake/common/unused.h"
 
+using Eigen::Isometry3d;
+
 namespace DrakeCollision {
 
-// TODO(jamiesnape): Implement the model.
+// TODO(avalenzu): Implement the model.
+
+// TODO(avalenzu): Tailor DistanceData and distanceFunction to our use-case
+
+/// @brief Distance data stores the distance request and the result given by distance algorithm.
+struct DistanceData
+{
+  /// @brief Distance request
+  fcl::DistanceRequestd request;
+
+  /// @brief Vector of distance results
+  std::vector<PointPair>* closest_points{nullptr};
+};
+
+/// @brief Default distance callback for two objects o1 and o2 in broad phase. return value means whether the broad phase can stop now. also return dist, i.e. the bmin distance till now
+bool allToAllDistanceFunction(fcl::CollisionObjectd* o1,
+    fcl::CollisionObjectd* o2, void* cdata_, double& dist)
+{
+  auto element1 = static_cast<Element*>(o1->getUserData());
+  auto element2 = static_cast<Element*>(o1->getUserData());
+  if (element1 && element2 && element1->CanCollideWith(element2)) {
+      auto* cdata = static_cast<DistanceData*>(cdata_);
+      const fcl::DistanceRequestd& request = cdata->request;
+      fcl::DistanceResultd result;
+
+      distance(o1, o2, request, result);
+
+      double distance = result.min_distance;
+      //auto normal = result.nearest_points[0]
+      //cdata->closest_points->emplace_back(element1, element2, 
+          //result.nearest_points[0], result.nearest_points[1], 
+
+  }
+  return false; // Check all N^2 pairs
+}
 
 void FCLModel::DoAddElement(const Element& element) {
-  drake::unused(element);
-  DRAKE_ABORT_MSG("Not implemented.");
 
   ElementId id = element.getId();
 
   if (id != 0) {
     // Create the fcl::CollisionObject
-    std::shared_ptr<fcl::CollisionGeometry<double>> fcl_geometry;
+    std::shared_ptr<fcl::CollisionGeometryd> fcl_geometry;
 
     switch (element.getShape()) {
       case DrakeShapes::BOX: {
         const auto box =
             static_cast<const DrakeShapes::Box&>(element.getGeometry());
-        fcl_geometry = std::shared_ptr<fcl::CollisionGeometry<double>>(new fcl::Box<double>(box.size));
+        fcl_geometry = std::shared_ptr<fcl::CollisionGeometryd>(new fcl::Boxd(box.size));
       } break;
       case DrakeShapes::SPHERE: {
         const auto sphere =
             static_cast<const DrakeShapes::Sphere&>(element.getGeometry());
-        fcl_geometry = std::shared_ptr<fcl::CollisionGeometry<double>>(new fcl::Sphere<double>(sphere.radius));
+        fcl_geometry = std::shared_ptr<fcl::CollisionGeometryd>(new fcl::Sphered(sphere.radius));
       } break;
       case DrakeShapes::CYLINDER: {
         const auto cylinder =
             static_cast<const DrakeShapes::Cylinder&>(element.getGeometry());
-        fcl_geometry = std::shared_ptr<fcl::CollisionGeometry<double>>(new fcl::Cylinder<double>(cylinder.radius, cylinder.length));
+        fcl_geometry = std::shared_ptr<fcl::CollisionGeometryd>(new fcl::Cylinderd(cylinder.radius, cylinder.length));
       } break;
       case DrakeShapes::MESH: {
         DRAKE_ABORT_MSG("Not implemented.");
@@ -44,7 +79,7 @@ void FCLModel::DoAddElement(const Element& element) {
       case DrakeShapes::CAPSULE: {
         const auto capsule =
             static_cast<const DrakeShapes::Capsule&>(element.getGeometry());
-        fcl_geometry = std::shared_ptr<fcl::CollisionGeometry<double>>(new fcl::Capsule<double>(capsule.radius, capsule.length));
+        fcl_geometry = std::shared_ptr<fcl::CollisionGeometryd>(new fcl::Capsuled(capsule.radius, capsule.length));
       } break;
       default:
         DRAKE_ABORT_MSG("Not implemented.");
@@ -55,7 +90,10 @@ void FCLModel::DoAddElement(const Element& element) {
     }
     
     if (fcl_geometry != nullptr) {
-      std::unique_ptr<fcl::CollisionObject<double>> fcl_object{new fcl::CollisionObject<double>(fcl_geometry)};
+      std::unique_ptr<fcl::CollisionObjectd> fcl_object{new fcl::CollisionObjectd(fcl_geometry)};
+
+      // Store a pointer to the Element in the fcl collision object
+      fcl_object->setUserData(FindMutableElement(id));
       // Register the object with the collision manager
       broadphase_manager_.registerObject(fcl_object.get());
       // Take ownership of FCL collision object
@@ -80,8 +118,6 @@ void FCLModel::updateModel() {
 bool FCLModel::closestPointsAllToAll(const std::vector<ElementId>& ids_to_check,
                                      bool use_margins,
                                      std::vector<PointPair>& closest_points) {
-  drake::unused(ids_to_check, use_margins, closest_points);
-  DRAKE_ABORT_MSG("Not implemented.");
   return false;
 }
 
