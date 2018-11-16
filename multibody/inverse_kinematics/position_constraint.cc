97f9b994a3c23882b4ee7050ca45143be405cb44
Andrés Valenzuela <andres.valenzuela@tri.global>
2018-11-16 16:15:14 -0500

    Convert PositionConstraint to use MultibodyPlant<double>.

--- multibody/inverse_kinematics/position_constraint.cc
+++ multibody/inverse_kinematics/position_constraint.cc
@@ -7,16 +7,16 @@ namespace drake {
 namespace multibody {
 namespace internal {
 PositionConstraint::PositionConstraint(
-    const multibody::MultibodyTree<AutoDiffXd>& tree,
+    const multibody_plant::MultibodyPlant<double>& plant,
     const FrameIndex& frameB_idx, const Eigen::Ref<const Eigen::Vector3d>& p_BQ,
     const FrameIndex& frameA_idx,
     const Eigen::Ref<const Eigen::Vector3d>& p_AQ_lower,
     const Eigen::Ref<const Eigen::Vector3d>& p_AQ_upper,
-    MultibodyTreeContext<AutoDiffXd>* context)
-    : solvers::Constraint(3, tree.num_positions(), p_AQ_lower, p_AQ_upper),
-      tree_(tree),
-      frameB_{tree_.get_frame(frameB_idx)},
-      frameA_{tree_.get_frame(frameA_idx)},
+    systems::Context<double>* context)
+    : solvers::Constraint(3, plant.num_positions(), p_AQ_lower, p_AQ_upper),
+      plant_(plant),
+      frameB_{plant_.tree().get_frame(frameB_idx)},
+      frameA_{plant_.tree().get_frame(frameA_idx)},
       p_BQ_{p_BQ},
       context_{context} {
   DRAKE_DEMAND(context);
@@ -32,9 +32,21 @@ void PositionConstraint::DoEval(const Eigen::Ref<const Eigen::VectorXd>& x,
 void PositionConstraint::DoEval(const Eigen::Ref<const AutoDiffVecXd>& x,
                                 AutoDiffVecXd* y) const {
   y->resize(3);
-  UpdateContextConfiguration(x, context_);
-  tree_.CalcPointsPositions(*context_, frameB_, p_BQ_.cast<AutoDiffXd>(),
-                            frameA_, y);
+  UpdateContextConfiguration(context_, plant_, math::autoDiffToValueMatrix(x));
+  Eigen::Vector3d p_AQ{};
+  plant_.tree().CalcPointsPositions(*context_, frameB_, p_BQ_, frameA_, &p_AQ);
+  Eigen::MatrixXd Jv_WBq(3, plant_.num_positions());
+  Eigen::MatrixXd Jv_WA(3, plant_.num_positions());
+  plant_.tree().CalcPointsGeometricJacobianExpressedInWorld(*context_, frameB_,
+                                                            p_BQ_, &Jv_WBq);
+  plant_.tree().CalcPointsGeometricJacobianExpressedInWorld(
+      *context_, frameA_, Eigen::Vector3d::Zero(), &Jv_WA);
+  Eigen::Matrix3d R_AW =
+      plant_.tree()
+          .CalcRelativeTransform(*context_, frameA_, plant_.world_frame())
+          .linear();
+  Eigen::Matrix3Xd Jv_ABq = R_AW * (Jv_WBq - Jv_WA);
+  *y = math::initializeAutoDiffGivenGradientMatrix(p_AQ, Jv_ABq);
 }
 
 }  // namespace internal
